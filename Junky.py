c9b954399f62182d4c9c2bbcd2a49298d0445234
Kixiron <25047011+Kixiron@users.noreply.github.com>
2019-01-27 20:23:25 -0600

    Fixed for less errors

--- Junky.py
+++ Junky.py
@@ -97,7 +97,7 @@ class Junky():
 
     def generate_class(self):
         junk = "/* START JUNK */\n"
-        junk += "private class {}_class\n".format(self.generate_var())
+        junk += "class {}_class\n".format(self.generate_var())
         junk += "{{\n"
         for i in range(random.randint(1, 3)):
             junk += self.generate_global_var()
@@ -132,7 +132,7 @@ class Junky():
         return junk
 
     def generate_imports(self):
-        imports = [ "System", "System.CodeDom", "System.Collections", "System.ComponentModel", "System.Configuration", "System.Deployment", "System.Diagnostics", "System.Diagnostics.CodeAnalysis", "System.Diagnostics.Contracts", "System.Diagnostics.Eventing", "System.Diagnostics.Tracing", "System.Diagnostics.PreformanceData", "System.Dynamic", "System.Globalization", "System.IO", "System.Linq", "System.Management", "System.Media", "System.Net", "System.Reflection", "System.Timers","System.Timers","System.Threading","System.Text","System.Text.RegularExpressions", "System.Security", "System.Runtime", "System.Resources", "System.Windows" ]
+        imports = [ "System", "System.CodeDom", "System.Collections", "System.ComponentModel", "System.Configuration", "System.Deployment", "System.Diagnostics", "System.Diagnostics.CodeAnalysis", "System.Diagnostics.Contracts", "System.Diagnostics.Eventing", "System.Diagnostics.Tracing", "System.Diagnostics.PerformanceData", "System.Dynamic", "System.Globalization", "System.IO", "System.Linq", "System.Management", "System.Media", "System.Net", "System.Reflection", "System.Timers","System.Timers","System.Threading","System.Text","System.Text.RegularExpressions", "System.Security", "System.Runtime", "System.Resources", "System.Windows" ]
         junk = "/* START JUNK */\n"
         for i in range(random.randint(1, 5)):
             junk += "using " + random.choice(imports) + ";\n"
@@ -145,7 +145,7 @@ class Junky():
         values = [ "int", "uint", "long", "ulong", "byte", "short", "bool" ]
         condition = ""
         for i in range(random.randint(1, 3)):
-            condition += " ({} {} {}) ".format(self.generate_ulong(), random.choice(operand_types), self.generate_ulong())
+            condition += " ((ulong){} {} {}) ".format(self.generate_ulong(), random.choice(operand_types), self.generate_ulong())
         condition = condition.replace("  ", " " + random.choice(conditionals) + " ").strip()
         junk = "if ({})".format(condition)
         junk += "{{\n"
@@ -176,7 +176,7 @@ class Junky():
         values = [ "int", "uint", "long", "ulong", "byte", "short", "bool" ]
         condition = ""
         for i in range(random.randint(1, 4)):
-            condition += " ({} {} {}) ".format(self.generate_ulong(), random.choice(operand_types), self.generate_ulong())
+            condition += " ((ulong){} {} {}) ".format(self.generate_ulong(), random.choice(operand_types), self.generate_ulong())
         condition = condition.replace("  ", " " + random.choice(conditionals) + " ").strip()
         junk = "if ({})".format(condition)
         junk += "{{\n"
@@ -202,7 +202,7 @@ class Junky():
     def generate_for(self):
         operand_types = [ "<=", ">=", ">", "<" ]
         for_var = self.generate_var()
-        junk = "for (var {} = 0; {} {} {}; {}++)\n".format(for_var, for_var, random.choice(operand_types), self.generate_ulong(), for_var)
+        junk = "for (var {} = 0; (uint){} {} {}; {}++)\n".format(for_var, for_var, random.choice(operand_types), self.generate_ulong(), for_var)
         junk += "{{\n"
         for i in range(random.randint(1, 3)):
             junk += self.generate_if()
@@ -212,7 +212,7 @@ class Junky():
     def generate_safe_for(self):
         operand_types = [ "<=", ">=", ">", "<" ]
         for_var = self.generate_var()
-        junk = "for (var {} = 0; {} {} {}; {}++)\n".format(for_var, for_var, random.choice(operand_types), self.generate_ulong(), for_var)
+        junk = "for (var {} = 0; (uint){} {} {}; {}++)\n".format(for_var, for_var, random.choice(operand_types), self.generate_ulong(), for_var)
         junk += "{{\n"
         for i in range(random.randint(1, 2)):
             junk += self.generate_safe_if()
@@ -220,7 +220,7 @@ class Junky():
         return junk
 
     def generate_enum(self):
-        values = [ "int", "uint", "long", "ulong", "byte", "short", "bool" ]
+        values = [ "int", "uint", "long", "ulong", "byte", "short" ]
         junk = "[Flags]\n"
         value = random.choice(values)
         junk += "private enum {}_enum : {}\n".format(self.generate_var(), value)
@@ -238,8 +238,6 @@ class Junky():
                 generator = self.generate_byte()
             elif value == "short":
                 generator = self.generate_short()
-            elif value == "bool":
-                generator = self.generate_bool()
             junk += "{} = {},\n".format(self.generate_var(), generator)
         junk += "}}\n"
         return junk
@@ -252,7 +250,7 @@ class Junky():
         return junk
 
     def generate_global_var(self):
-        var_types = [ "string", "string[]", "int", "int[]", "long", "long[]", "ulong", "ulong[]", "UIntPtr", "UIntPtr[]", "IntPtr", "IntPtr[]" "bool", "bool[]", "var", "var[]" "sbyte", "sbyte[]", "short", "short[]", "byte", "byte[]", "ushort", "ushort[]", "uint", "uint[]", "char", "char[]", "float", "float[]", "double", "double[]", "decimal", "decimal[]", "object",  "object[]" ]
+        var_types = [ "string", "string[]", "int", "int[]", "long", "long[]", "ulong", "ulong[]", "UIntPtr", "UIntPtr[]", "IntPtr", "IntPtr[]", "bool", "bool[]", "sbyte", "sbyte[]", "short", "short[]", "byte", "byte[]", "ushort", "ushort[]", "uint", "uint[]", "char", "char[]", "float", "float[]", "double", "double[]", "decimal", "decimal[]", "object",  "object[]" ]
         value = random.choice(var_types)
         if value == "int":
             return "private static {} {} = {};\n".format(value, self.generate_var() , self.generate_int())
@@ -278,13 +276,13 @@ class Junky():
         return random.randint(-2147483647, 2147483647)
 
     def generate_uint(self):
-        return random.randint(-4294967295, 4294967295)
+        return random.randint(0, 4294967295)
 
     def generate_long(self):
-        return random.randint(-9223372036854775807, 9223372036854775807)
+        return random.randint(0, 9223372036854775807)
 
     def generate_ulong(self):
-        return random.randint(-18446744073709551615, 18446744073709551615)
+        return random.randint(0, 18446744073709551615)
 
     def generate_byte(self):
         return random.randint(0, 255)
@@ -293,7 +291,7 @@ class Junky():
         return random.randint(-32768, 32768)
 
     def generate_bool(self):
-        return random.choice([ "true", "false", "null" ])
+        return random.choice([ "true", "false" ])
 
 def main():
     path = input("File Directory:\n")
